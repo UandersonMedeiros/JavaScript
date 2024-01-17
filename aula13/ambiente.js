6e24c8abad3fb8d325b85fdfb3967bd4ff9447c7
Uanderson Medeiros de Lacerda <152915267+UandersonMedeiros@users.noreply.github.com>
2024-01-17 17:41:16 -0300

    .

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+/* while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ // mesma coisa c = c +1
+}
+
+         OU 
+*/
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+// console.log(`Passo ${c}`)
+
+/* 
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+*/
